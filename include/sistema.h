6220d952e88b9f914aec4de10b59669ef9572b3a
Julio <julio.engcomp@gmail.com>
2021-07-13 14:39:31 -0300

    Update sistema.h

--- include/sistema.h
+++ include/sistema.h
@@ -15,7 +15,7 @@ class Sistema {
   private:
 		std::vector<Servidor> servidores; //<! um vetor com todos os servidores
 		std::vector<Usuario> usuarios; //<! um vetor com todos os usuários cadastrados
-		std::map< int, std::pair<Servidor*, CanalTexto*> > usuariosLogados; //<! um vetor contendo os usuários que logaram no sistema
+		std::map< int, std::pair<std::string, std::string> > usuariosLogados; //<! um vetor contendo os usuários que logaram no sistema
 		
 
   public:
