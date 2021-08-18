114387e8bf9c9492f5a8afcb5cdbfcb223014272
Débora Azevedo <deboraazevedoo@gmail.com>
2021-08-18 10:53:31 -0300

    adicionando as classes de canal de texto e servidor

--- include/canaltexto.h
+++ include/canaltexto.h
@@ -1,7 +1,14 @@
 #ifndef CANALTEXTO_H
 #define CANALTEXTO_H
+#include <vector>
+#include <string>
+#include "mensagem.h"
+
 
 class CanalTexto{
+	string nome;
+	vector<Mensagem> mensagens;
+
 	/*
 		TODO implementação dos atributos e métodos
 	*/
--- include/servidor.h
+++ include/servidor.h
@@ -1,7 +1,16 @@
 #ifndef SERVIDOR_H
 #define SERVIDOR_H
+#include <string>
+#include "canaltexto.h"
+#include <vector>
 
 class Servidor{
+	int usuarioDonoId;
+	string nome;
+	string descricao;
+	string codigoConvite;
+	vector<CanalTexto> canaisTexto;
+	vector<int> participantesIDs;
 	/*
 		TODO implementação dos atributos e métodos
 	*/
